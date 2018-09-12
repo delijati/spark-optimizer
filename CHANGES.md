337aff2fe2ab7cdbd4c14c93344e40ae0c9f3043
Josip Delic <delijati@googlemail.com>
2018-09-12 22:49:56 +0200

    Back to development: 0.1.1

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 # CHANGES
 
+0.1.1 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 0.1.0 (2018-09-12)
 
 - initial release
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 
 setup(
     name='spark_optimizer',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description='Optimize AWS EMR spark settings (spark-config-cheatsheet)',
     long_description=README + '\n\n' + CHANGES,
     author='Josip Delic',
